2076aa2e89952cba4566182dd89ab201f96a8d06
Agustin-dotcom <Agustin.Prieto2@alu.uclm.es>
2025-09-25 10:11:45 +0200

    first commit

---
+++ src/models/Product.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema, Types } from 'mongoose';
+
+  export interface CartItem {
+    product: Types.ObjectId;
+    qty: number;
+  }
+  
+  export interface Product {
+    name: string;
+    description: string;
+    img: string;
+    price: number;
+  }
+  
+  const ProductSchema = new Schema<Product>({
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  });
+
+  export default mongoose.models.Product as mongoose.Model<Product> || mongoose.model<Product>('Product', ProductSchema);
