09eed0b667daea8bb98e7aec1007d1c0a42cd605
santiago <santiposso2009@gmail.com>
2025-10-08 09:55:24 +0200

    practice 1 complete

--- src/lib/handlers.ts
+++ src/lib/handlers.ts
@@ -282,15 +282,11 @@ export async function createOrder(
 
   // Transform cart items to order items (with price snapshot)
   const orderItems = user.cartItems.map((cartItem) => {
-    const product = cartItem.product; // populated
-    const productProjection = {
-      price:true
-    }
-    const pr = Products.findById(cartItem.product,productProjection)
+    const product = cartItem.product as any; // populated
     return {
       product: product._id,
       qty: cartItem.qty,
-      price: pr, // snapshot current price
+      price: product.price, // snapshot current price
     };
   });
 
