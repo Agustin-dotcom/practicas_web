3efb304a441577e5d7b5b212ce99869c20f26b2f
santiago <santiposso2009@gmail.com>
2025-10-30 11:17:31 +0100

    diseno responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
-    <header className='mx-auto w-full bg-[#ff8000] px-6 pb-16 pt-24 text-center sm:pb-20 sm:pt-28 lg:px-8 lg:pb-24 lg:pt-32'>
+    <header className='mx-auto w-full bg-[#ff8000] p-4 pb-16 pt-24 text-center sm:pb-20 sm:pt-28 lg:px-8 lg:pb-24 lg:pt-32'>
       <div className='mx-auto max-w-2xl'>
-        <h1 className='text-6xl font-bold text-gray-100 sm:text-7xl lg:text-8xl'>
+        <h1 className='text-center text-5xl font-bold text-gray-100 sm:text-7xl lg:text-8xl'>
           Supermarket
         </h1>
-        <p className='mt-4 text-sm leading-8 text-gray-400 sm:mt-6 sm:text-base lg:text-lg'>
+        <p className=' text-center mt-4 text-sm leading-8 text-gray-400 sm:mt-6 sm:text-base lg:text-lg'>
           The best Colombian supermarket in your area
         </p>
       </div>
